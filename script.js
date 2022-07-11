7cd80c585859b1c411415038b3cf29500b6b174d
Emily Mitcheson-Smith <emilyms25@hotmail.co.uk>
2022-07-11 14:54:23 +0100

    Added translate on button click

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ import {translateEnglishToMorse} from "./javascript/english-morse.js";
 
 const translatorInput = document.querySelector(".input-box");
 const translatorOutput = document.querySelector(".translator-output");
+const EngToMorseButton = document.querySelector(".buttonEngMorse");
 
 const displayEnglishToMorseTranslation = () => {
-    const input = event.target.value;
+    const input = translatorInput.value;
     translatorOutput.innerText = translateEnglishToMorse(input);
 }
 
-translatorInput.addEventListener("change", displayEnglishToMorseTranslation);
+EngToMorseButton.addEventListener("click", displayEnglishToMorseTranslation);
