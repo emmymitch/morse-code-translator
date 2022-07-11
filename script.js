cf30520a802b99a40bec9bc3b340300a62768b2b
Emily Mitcheson-Smith <emilyms25@hotmail.co.uk>
2022-07-11 16:07:03 +0100

    Added functionality for morse-eng

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 import {morseDictionary} from "./javascript/morse-dictionary.js";
 import {translateEnglishToMorse} from "./javascript/english-morse.js";
+import { translateMorseToEnglish } from "./javascript/morse-english.js";
 
 const translatorInput = document.querySelector(".input-box");
 const translatorOutput = document.querySelector(".translator-output");
 const EngToMorseButton = document.querySelector("#buttonEngMorse");
+const MorseToEngButton = document.querySelector("#buttonMorseEng");
 
 const displayEnglishToMorseTranslation = () => {
     const input = translatorInput.value;
     translatorOutput.innerText = translateEnglishToMorse(input);
 }
 
+const displayMorseToEnglishTranslation = () => {
+    const input = translatorInput.value;
+    translatorOutput.innerText = translateMorseToEnglish(input);
+}
+
 EngToMorseButton.addEventListener("click", displayEnglishToMorseTranslation);
+MorseToEngButton.addEventListener("click", displayMorseToEnglishTranslation);
