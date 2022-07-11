b38e4b39968436d8202bdb0d4e62d425c188c6cf
Emily Mitcheson-Smith <emilyms25@hotmail.co.uk>
2022-07-11 12:47:40 +0100

    Added tests

--- script.spec.js
+++ script.spec.js
@@ -1,7 +1,7 @@
 import {expect, it} from "@jest/globals";
 import {translate} from "./script";
 
-
+//Positive tests
 it("Should translate 'Hello' to ....|.|.-..|.-..|---", () => {
     const result = translate("Hello");
     expect(result).toBe("....|.|.-..|.-..|---")
@@ -17,7 +17,24 @@ it("Should translate special characters", () => {
     expect(result).toBe("  -....-|-..-.|-...-|---...|.-.-.-|..--..|.--.-.|.-...|.-..-.|..--.-|-.-.--|-.--.-");
 })
 
+it("Should translate sentences", () => {
+    const result = translate("This is a sentence.");
+    expect(result).toBe("-|....|..|...||..|...||.-||...|.|-.|-|.|-.|-.-.|.||.-.-.-");
+})
+
 it("Should return a string", () => {
     const result = translate("Hello");
     expect(typeof result).toBe("string");
+})
+
+
+//Negative tests
+it("Should translate unknown characters as unknown", () => {
+    const result = translate("touché");
+    expect(result).toBe("-|---|..-|-.-.|....|unknown");
+})
+
+it("Should request input when none given", () => {
+    const result = translate();
+    expect(result).toBe("Please input text");
 })
